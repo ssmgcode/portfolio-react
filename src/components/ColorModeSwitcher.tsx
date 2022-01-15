cf86df0242bc1078a0268c1e84bab4169838869b
SSMG Code <ssmg.sg@gmail.com>
2022-01-15 08:51:56 -0600

    Initial commit

--- src/components/ColorModeSwitcher.tsx
+++ src/components/ColorModeSwitcher.tsx
@@ -1,18 +1,19 @@
-import * as React from "react"
+import * as React from 'react'
 import {
   useColorMode,
   useColorModeValue,
   IconButton,
   IconButtonProps,
-} from "@chakra-ui/react"
-import { FaMoon, FaSun } from "react-icons/fa"
+} from '@chakra-ui/react'
+// import { FaMoon, FaSun } from "react-icons/fa"
+import { HiSun, HiMoon } from 'react-icons/hi'
 
-type ColorModeSwitcherProps = Omit<IconButtonProps, "aria-label">
+type ColorModeSwitcherProps = Omit<IconButtonProps, 'aria-label'>
 
 export const ColorModeSwitcher: React.FC<ColorModeSwitcherProps> = (props) => {
   const { toggleColorMode } = useColorMode()
-  const text = useColorModeValue("dark", "light")
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun)
+  const text = useColorModeValue('dark', 'light')
+  const SwitchIcon = useColorModeValue(HiMoon, HiSun)
 
   return (
     <IconButton
@@ -22,7 +23,7 @@ export const ColorModeSwitcher: React.FC<ColorModeSwitcherProps> = (props) => {
       color="current"
       marginLeft="2"
       onClick={toggleColorMode}
-      icon={<SwitchIcon />}
+      icon={<SwitchIcon size={24} />}
       aria-label={`Switch to ${text} mode`}
       {...props}
     />
