c6dae92677a4c67426e21c3af691d89b5521f63e
SSMG Code <ssmg.sg@gmail.com>
2022-01-18 00:47:46 -0600

    Add react-router-dom

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,18 @@
-import * as React from 'react'
-import { ChakraProvider, theme } from '@chakra-ui/react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+import Home from './pages/Home'
+import HeaderBar from './sections/HeaderBar'
+import theme from '../theme'
 
-export const App = () => <ChakraProvider theme={theme}></ChakraProvider>
+export const App = () => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Router>
+        <HeaderBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
+  )
+}
