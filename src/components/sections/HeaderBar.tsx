14fd41732a96af6993b9fbebdf5b08dcdfbf56f2
SSMG Code <ssmg.sg@gmail.com>
2022-01-18 15:39:53 -0600

    Change HeaderBar styles

--- src/components/sections/HeaderBar.tsx
+++ src/components/sections/HeaderBar.tsx
@@ -1,29 +1,22 @@
 import { Box, IconButton, HStack, Link, Tooltip } from '@chakra-ui/react'
-import { HiCog } from 'react-icons/hi'
+import { HiDotsCircleHorizontal } from 'react-icons/hi'
 import { Link as RouterLink } from 'react-router-dom'
 import SSMGLogo from '../SSMGLogo'
 
 const NavBar = (): JSX.Element => {
   return (
-    <Box
-      height="60px"
-      padding=".5em"
-      paddingX="1.5em"
-      boxShadow="lg"
-      background="blue.600"
-    >
+    <Box height="70px" padding=".5em" paddingX="1.5em" boxShadow="xs">
       <HStack height="100%" justifyContent="space-between">
-        <Tooltip label="Go to home" openDelay={500}>
-          <Link as={RouterLink} to="/" height="100%">
-            <SSMGLogo fill="#ffffff" />
+        <Tooltip label="Go home" openDelay={500}>
+          <Link as={RouterLink} to="/" height="80%">
+            <SSMGLogo />
           </Link>
         </Tooltip>
         <Tooltip label="Settings">
           <IconButton
             aria-label="Settings"
-            icon={<HiCog size="80%" />}
+            icon={<HiDotsCircleHorizontal size="80%" />}
             variant="ghost"
-            color="white"
             size="lg"
           />
         </Tooltip>
