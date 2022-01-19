c997b337148b63d576d0969f4b5962d62dee3225
SSMG Code <ssmg.sg@gmail.com>
2022-01-19 09:10:11 -0600

    Create Grid structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Grid, GridItem } from '@chakra-ui/react'
 import Home from './pages/Home'
 import HeaderBar from './sections/HeaderBar'
 import theme from '../theme'
+import NavBar from './sections/NavBar'
 
 export const App = () => {
   return (
     <ChakraProvider theme={theme}>
       <Router>
-        <HeaderBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <Grid
+          height="100vh"
+          templateColumns="1fr"
+          templateRows="auto 1fr auto"
+          templateAreas={`
+            'header'
+            'main'
+            'footer'
+          `}
+        >
+          <GridItem gridArea="header">
+            <HeaderBar />
+          </GridItem>
+          <GridItem as="main" gridArea="main" overflow="auto">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </GridItem>
+          <GridItem gridArea="footer">
+            <NavBar />
+          </GridItem>
+        </Grid>
       </Router>
     </ChakraProvider>
   )
