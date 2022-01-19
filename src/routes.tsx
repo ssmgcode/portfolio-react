7b5657a59b35803d7832930f3c88ea7f645995a9
SSMG Code <ssmg.sg@gmail.com>
2022-01-19 11:24:29 -0600

    Create routes

---
+++ src/routes.tsx
@@ -0,0 +1,34 @@
+import { HiBriefcase, HiHome, HiPhone } from 'react-icons/hi'
+import Contact from './components/pages/Contact'
+import Home from './components/pages/Home'
+import Projects from './components/pages/Projects'
+
+interface AppRoute {
+  path: string
+  name: string
+  element: JSX.Element
+  icon: JSX.Element
+}
+
+const appRoutes: AppRoute[] = [
+  {
+    path: '/',
+    name: 'Home',
+    element: <Home />,
+    icon: <HiHome />,
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    element: <Contact />,
+    icon: <HiPhone />,
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    element: <Projects />,
+    icon: <HiBriefcase />,
+  },
+]
+
+export default appRoutes
