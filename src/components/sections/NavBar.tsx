55425ec3d6b0e1a465b673da3fb2a1f08a113456
SSMG Code <ssmg.sg@gmail.com>
2022-01-19 11:25:02 -0600

    Create NavBar

---
+++ src/components/sections/NavBar.tsx
@@ -0,0 +1,40 @@
+import { Box, HStack, Link, Text } from '@chakra-ui/react'
+import { Link as RouterLink } from 'react-router-dom'
+import appRoutes from '../../routes'
+import { useLocation } from 'react-router-dom'
+
+const NavBar = (): JSX.Element => {
+  const location = useLocation()
+
+  return (
+    <Box as="nav" height="60px" boxShadow="xs">
+      <HStack height="100%" justifyContent="space-evenly">
+        {appRoutes.map(({ name, path, icon }) => (
+          <Box
+            key={path}
+            background={location.pathname === path ? 'blue.500' : 'unset'}
+            padding=".3em"
+            borderRadius="base"
+          >
+            <Link
+              as={RouterLink}
+              to={path}
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              color={location.pathname === path ? 'white' : 'unset'}
+            >
+              {icon}
+              <Text fontSize=".9em" fontWeight="semibold">
+                {name}
+              </Text>
+            </Link>
+          </Box>
+        ))}
+      </HStack>
+    </Box>
+  )
+}
+
+export default NavBar
