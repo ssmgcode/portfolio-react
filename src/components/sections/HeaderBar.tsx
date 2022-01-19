2a41142333ec1a4606e8645f96cb94795e6a4488
SSMG Code <ssmg.sg@gmail.com>
2022-01-19 09:10:48 -0600

    Add a background to HeaderBar

--- src/components/sections/HeaderBar.tsx
+++ src/components/sections/HeaderBar.tsx
@@ -1,11 +1,29 @@
-import { Box, IconButton, HStack, Link, Tooltip } from '@chakra-ui/react'
+import {
+  Box,
+  IconButton,
+  HStack,
+  Link,
+  Tooltip,
+  useColorModeValue,
+} from '@chakra-ui/react'
 import { HiDotsCircleHorizontal } from 'react-icons/hi'
 import { Link as RouterLink } from 'react-router-dom'
 import SSMGLogo from '../SSMGLogo'
 
-const NavBar = (): JSX.Element => {
+interface Props {
+  children?: JSX.Element
+}
+
+const HeaderBar = ({ children }: Props): JSX.Element => {
+  const background = useColorModeValue('white', 'gray.800')
   return (
-    <Box height="70px" padding=".5em" paddingX="1.5em" boxShadow="xs">
+    <Box
+      height="70px"
+      padding=".5em"
+      paddingX="1.5em"
+      boxShadow="xs"
+      background={background}
+    >
       <HStack height="100%" justifyContent="space-between">
         <Tooltip label="Go home" openDelay={500}>
           <Link as={RouterLink} to="/" height="80%">
@@ -25,4 +43,4 @@ const NavBar = (): JSX.Element => {
   )
 }
 
-export default NavBar
+export default HeaderBar
