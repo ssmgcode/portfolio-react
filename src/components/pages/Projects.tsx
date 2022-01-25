c436c104bf09c032c087c0f743484491c5592dda
SSMG Code <ssmg.sg@gmail.com>
2022-01-25 08:58:07 -0600

    Implement simple grid

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,18 +1,19 @@
-import { Grid, Heading } from '@chakra-ui/react'
+import { Heading, SimpleGrid } from '@chakra-ui/react'
 import Project from '../sections/projects/Project'
 import personalInformation from '../../config/personalInformation.json'
+import DefaultPadding from '../layouts/DefaultPadding'
 
 const projects = personalInformation.projects
 
 const Projects = (): JSX.Element => (
-  <>
+  <DefaultPadding>
     <Heading>Projects</Heading>
-    <Grid templateColumns="auto" templateRows="auto" justifyItems="center">
+    <SimpleGrid minChildWidth="20em" spacing="2em">
       {projects.map((project) => (
         <Project key={project.name} {...project} />
       ))}
-    </Grid>
-  </>
+    </SimpleGrid>
+  </DefaultPadding>
 )
 
 export default Projects
