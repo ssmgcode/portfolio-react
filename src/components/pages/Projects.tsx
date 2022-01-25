3612009dc14230554acf160a69ac6e56dfebede3
SSMG Code <ssmg.sg@gmail.com>
2022-01-25 14:09:07 -0600

    Add Outlet to render the child route

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -2,18 +2,22 @@ import { Heading, SimpleGrid } from '@chakra-ui/react'
 import Project from '../sections/projects/Project'
 import personalInformation from '../../config/personalInformation.json'
 import DefaultPadding from '../layouts/DefaultPadding'
+import { Outlet } from 'react-router-dom'
 
 const projects = personalInformation.projects
 
 const Projects = (): JSX.Element => (
-  <DefaultPadding>
-    <Heading>Projects</Heading>
-    <SimpleGrid minChildWidth="20em" spacing="2em">
-      {projects.map((project) => (
-        <Project key={project.name} {...project} />
-      ))}
-    </SimpleGrid>
-  </DefaultPadding>
+  <>
+    <DefaultPadding>
+      <Heading>Projects</Heading>
+      <SimpleGrid minChildWidth="20em" spacing="2em">
+        {projects.map((project) => (
+          <Project key={project.name} {...project} />
+        ))}
+      </SimpleGrid>
+    </DefaultPadding>
+    <Outlet />
+  </>
 )
 
 export default Projects
