37ddac9ae806f8067e63b814108df5d1c8a152da
SSMG Code <ssmg.sg@gmail.com>
2022-01-25 08:06:20 -0600

    Improve design in home card

--- src/components/sections/Home/HomeCard.tsx
+++ src/components/sections/Home/HomeCard.tsx
@@ -7,7 +7,13 @@ interface Props {
 
 const HomeCard = ({ children }: Props): JSX.Element => {
   return (
-    <Box shadow="md" padding="1em" borderRadius="md" borderWidth="1px">
+    <Box
+      shadow="md"
+      padding="1em"
+      borderRadius="md"
+      borderWidth="1px"
+      borderBottomWidth="5px"
+    >
       {children}
     </Box>
   )
