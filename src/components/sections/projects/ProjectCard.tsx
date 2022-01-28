e1527f3710536d9b20f368e5c98d6a195204909a
SSMG Code <ssmg.sg@gmail.com>
2022-01-27 20:41:45 -0600

    Improve container

--- src/components/sections/projects/ProjectCard.tsx
+++ src/components/sections/projects/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Image, Text, Badge, Link } from '@chakra-ui/react'
+import { Box, Heading, Image, Text, Badge } from '@chakra-ui/react'
 import { Link as RouterLink } from 'react-router-dom'
 import Project from '../../../models/Project'
 
@@ -10,38 +10,37 @@ const ProjectCard = ({
   image,
 }: Project): JSX.Element => {
   return (
-    <Box sx={{ '.card:hover &': { translateY: '5px' } }}>
-      <RouterLink to={id} className="card">
-        <Box
-          maxWidth="25em"
-          borderRadius="md"
-          borderWidth="1px"
-          borderBottomWidth="5px"
-          transform="translateY(0)"
-          transition="ease"
-          transitionProperty="all"
-          transitionDuration=".1s"
-          sx={{
-            '.card:hover &': {
-              borderBottomWidth: '10px',
-              transform: 'translateY(-5px)',
-            },
-          }}
-        >
-          <Box borderTopRadius="md" overflow="hidden">
-            <Image src={image} alt={name} />
-          </Box>
-          <Box padding="1em">
-            <Heading
-              as="h5"
-              fontSize="2xl"
-              sx={{ '.card:hover &': { textDecoration: 'underline' } }}
-            >
-              {name}
-            </Heading>
-            <Text marginBottom=".5em">{description}</Text>
-            <Badge>{type}</Badge>
-          </Box>
+    <Box
+      maxWidth="25em"
+      borderRadius="md"
+      borderWidth="1px"
+      borderBottomWidth="5px"
+      transform="translateY(0)"
+      transition="ease"
+      transitionProperty="all"
+      transitionDuration=".1s"
+      className="card"
+      sx={{
+        '&:hover': {
+          borderBottomWidth: '10px',
+          transform: 'translateY(-5px)',
+        },
+      }}
+    >
+      <RouterLink to={id}>
+        <Box borderTopRadius="md" overflow="hidden">
+          <Image src={image} alt={name} />
+        </Box>
+        <Box padding="1em">
+          <Heading
+            as="h5"
+            fontSize="2xl"
+            sx={{ '.card:hover &': { textDecoration: 'underline' } }}
+          >
+            {name}
+          </Heading>
+          <Text marginBottom=".5em">{description}</Text>
+          <Badge>{type}</Badge>
         </Box>
       </RouterLink>
     </Box>
